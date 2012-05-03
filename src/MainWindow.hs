78ab45cefc84dc6b87d79e9bda4f1a3799991560
Rickard Lindberg <ricli85@gmail.com>
2012-05-03 14:27:26 +0200

    diagram: draw basic shape of diagram

--- src/MainWindow.hs
+++ src/MainWindow.hs
@@ -1,5 +1,6 @@
 module MainWindow (showMainWindow) where
 
+import Control.Monad
 import Data.IORef
 import Graphics.Rendering.Cairo hiding (status, Status)
 import Graphics.UI.Gtk
@@ -10,6 +11,9 @@ showMainWindow :: IO ()
 showMainWindow = do
     trackingRef   <- newIORef $ Tracking "foo" [ TrackingEntry 1 1 ""
                                                , TrackingEntry 2 2 ""
+                                               , TrackingEntry 3 1 ""
+                                               , TrackingEntry 4 2 ""
+                                               , TrackingEntry 5 1.5 ""
                                                ]
     builder       <- getDataFileName "interface.glade" >>= builderFromFile
     mainWindow    <- builderGetObject builder castToWindow            "main_window"
@@ -33,5 +37,47 @@ redraw canvas trackingRef event = do
 
 renderScreen :: Tracking -> Double -> Double -> Render ()
 renderScreen tracking w h = do
+    let border = 20
+    let (ox, oy) = (border, h - border)
+    let tw = w - 2 * border
+    let th = h - 2 * border
+
+    -- Background
     setSourceRGB 1 1 0.6
     paint
+
+    -- Axises
+    setSourceRGB 0 0 0
+    setLineWidth 2
+    -- X-axis
+    moveTo ox oy
+    lineTo (ox+tw) oy
+    stroke
+    -- Y-axis
+    moveTo ox oy
+    lineTo ox (oy-th)
+    stroke
+
+    -- Data points
+    let vMinTime  = fromIntegral $ minTime tracking
+    let vMaxTime  = fromIntegral $ maxTime tracking
+    let vMinValue = minValue tracking
+    let vMaxValue = maxValue tracking
+    let toX time  = ox + tw * (fromIntegral time - vMinTime)  / (vMaxTime  - vMinTime)
+    let toY value = oy - th * (value             - vMinValue) / (vMaxValue - vMinValue)
+    -- Line between points
+    setSourceRGBA 0 0 0 0.5
+    setLineWidth 1
+    forM_ (entries tracking) $ \entry -> do
+        let px = toX (time entry)
+        let py = toY (value entry)
+        lineTo px py
+    stroke
+    -- Points
+    setSourceRGB 0 0 0
+    forM_ (entries tracking) $ \entry -> do
+        let px = toX (time entry)
+        let py = toY (value entry)
+        let r = 3
+        arc px py r 0 (2*pi)
+        fill
--- src/Tracking.hs
+++ src/Tracking.hs
@@ -6,7 +6,19 @@ data Tracking = Tracking
     } deriving (Show, Eq)
 
 data TrackingEntry = TrackingEntry
-    { value   :: Double
-    , time    :: Int
+    { time    :: Int
+    , value   :: Double
     , comment :: String
     } deriving (Show, Eq)
+
+minTime :: Tracking -> Int
+minTime t = minimum $ map time (entries t)
+
+maxTime :: Tracking -> Int
+maxTime t = maximum $ map time (entries t)
+
+minValue :: Tracking -> Double
+minValue t = minimum $ map value (entries t)
+
+maxValue :: Tracking -> Double
+maxValue t = maximum $ map value (entries t)
